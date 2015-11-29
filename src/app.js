f3cfd955a5e062656cbafc811df68cfd72ef00ca
Jeremy Brown <jeremy@jeremysbrown.com>
2015-11-29 06:03:36 -0500

    navigation changes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 export class App {
-    message = 'Welcome to Aurelia!';
 
     configureRouter(config, router) {
         this.router = router;
 
+        config.title = "Aurelia Lab";
         config.map([
             {route:["", "home"], moduleId:"home", name:"home", nav:true, title:"Home"},
             {route:"bindingdemo", moduleId:"binding-demo", name:"bindingDemo", nav:false, title:"Binding", subtitle:"DEMO!"}
