6fcaf304be10fbb45c3c74975b56c43c7cbb3d9c
Jeremy Brown <jeremy@jeremysbrown.com>
2015-11-29 07:47:30 -0500

    route added

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ export class App {
         config.title = "Aurelia Lab";
         config.map([
             {route:["", "home"], moduleId:"home", name:"home", nav:true, title:"Home"},
-            {route:"bindingdemo", moduleId:"binding-demo", name:"bindingDemo", nav:false, title:"Binding", subtitle:"DEMO!"}
+            {route:"bindingdemo", moduleId:"binding-demo", name:"bindingDemo", nav:true, title:"Binding Demo"}
         ]);
     }
 }
